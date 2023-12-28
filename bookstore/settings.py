ddc72f7ed1f59324e9ca6e541140567fec939172
OBo1307 <och.bogdan@gmail.com>
2023-12-28 17:16:43 +0000

    Update ALLOWED_HOSTS with site and development server URL

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-=acj)ds1d1^he4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
